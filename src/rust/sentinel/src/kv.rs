10e48984d589b92d77b2bc401d3b6fe63801e8c6
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2023-07-05 08:37:14 +0700

    style: update with cargo fmt

--- src/rust/sentinel/src/kv.rs
+++ src/rust/sentinel/src/kv.rs
@@ -12,7 +12,9 @@ pub struct RocksDB {
 }
 impl KVStore for RocksDB {
     fn init(file_path: &str) -> Self {
-        RocksDB { db: Arc::new(DB::open_default(file_path).unwrap()) }
+        RocksDB {
+            db: Arc::new(DB::open_default(file_path).unwrap()),
+        }
     }
     fn save(&self, k: &str, v: &str) -> bool {
         self.db.put(k.as_bytes(), v.as_bytes()).is_ok()
@@ -23,11 +25,11 @@ impl KVStore for RocksDB {
                 let result = String::from_utf8(v).unwrap();
                 println!("Finding '{}' returns '{}'", k, result);
                 Some(result)
-            },
+            }
             Ok(None) => {
                 println!("Finding '{}' returns None", k);
                 None
-            },
+            }
             Err(e) => {
                 println!("Error retrieving value for {}: {}", k, e);
                 None
--- src/rust/sentinel/src/main.rs
+++ src/rust/sentinel/src/main.rs
@@ -4,26 +4,21 @@ mod kv_handler;
 async fn main() -> std::io::Result<()> {
     use actix_web::{
         middleware::Logger,
-        web::{scope, resource, get, post, delete},
-        App,
-        HttpServer
+        web::{delete, get, post, resource, scope},
+        App, HttpServer,
     };
     let db: kv::RocksDB = kv::KVStore::init("/tmp/rocks/actix-db");
     std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
     env_logger::init();
     HttpServer::new(move || {
-        App::new()
-            .data(db.clone())
-            .wrap(Logger::default())
-            .service(
-                scope("/api")
-                .service(
-                    resource("/{key}")
-                        .route(get().to(kv_handler::get))
-                        .route(post().to(kv_handler::post))
-                        .route(delete().to(kv_handler::delete)),
-                ),
-            )
+        App::new().data(db.clone()).wrap(Logger::default()).service(
+            scope("/api").service(
+                resource("/{key}")
+                    .route(get().to(kv_handler::get))
+                    .route(post().to(kv_handler::post))
+                    .route(delete().to(kv_handler::delete)),
+            ),
+        )
     })
     .bind("0.0.0.0:8080")?
     .run()
